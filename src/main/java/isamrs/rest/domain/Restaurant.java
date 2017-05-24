56c66faefd2e1b03f3422f987883c1dbf42bbe7f
Jovan <Jovan@DESKTOP-F489GJG>
2017-05-24 15:42:07 +0200

    message

--- src/main/java/isamrs/rest/domain/Restaurant.java
+++ src/main/java/isamrs/rest/domain/Restaurant.java
@@ -1,7 +1,15 @@
 package isamrs.rest.domain;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Restaurant{
 
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	private String type;
+	
 	public Restaurant(){
 		
 	}
--- src/main/java/isamrs/rest/domain/SystemManager.java
+++ src/main/java/isamrs/rest/domain/SystemManager.java
@@ -10,10 +10,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class SystemManager implements Serializable{
+public class SystemManager implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
@@ -27,42 +27,78 @@ public class SystemManager implements Serializable{
 	private String password;
 	@Column(nullable = false)
 	private int master;
-	
-	public SystemManager(){
-		
+
+	public SystemManager() {
+
 	}
-	public SystemManager(String email, String password){
+
+	public SystemManager(String email, String password) {
 		super();
 		this.email = email;
 		this.password = password;
 		this.master = 0;
 	}
-	public SystemManager(String firstname, String lastname, String email, String password){
+
+	public SystemManager(String firstname, String lastname, String email, String password) {
 		super();
 		this.firstname = firstname;
 		this.lastname = lastname;
 		this.email = email;
 		this.password = password;
 		this.master = 0;
-	} 
-	
-	public Long getId() { return id; };
-	public String getFirstname() { return this.firstname; };
-	public String getLastname() { return this.lastname; };
-	public String getEmail() { return this.email; };
-	public String getPassword() { return this.password; };
-	public int getMaster() { return this.master; };
-	
-	public void setId(Long id) { this.id = id; };
-	public void setFirstname(String firstname) { this.firstname = firstname; };
-	public void setLastname(String lastname) { this.lastname = lastname; };
-	public void setEmail(String email) { this.email = email; };
-	public void setPassword(String password) { this.password = password; };
-	public void setMaster(int master) { this.master = master; };
-	
+	}
+
+	public Long getId() {
+		return id;
+	};
+
+	public String getFirstname() {
+		return this.firstname;
+	};
+
+	public String getLastname() {
+		return this.lastname;
+	};
+
+	public String getEmail() {
+		return this.email;
+	};
+
+	public String getPassword() {
+		return this.password;
+	};
+
+	public int getMaster() {
+		return this.master;
+	};
+
+	public void setId(Long id) {
+		this.id = id;
+	};
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	};
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	};
+
+	public void setEmail(String email) {
+		this.email = email;
+	};
+
+	public void setPassword(String password) {
+		this.password = password;
+	};
+
+	public void setMaster(int master) {
+		this.master = master;
+	};
+
 	@Override
-	public String toString(){
-		return "System manager " + getFirstname() + " " + getLastname() 
-		+ " with email " + getEmail() + " and password " + StringUtils.repeat("*", getPassword().length());
+	public String toString() {
+		return "System manager " + getFirstname() + " " + getLastname() + " with email " + getEmail() + " and password "
+				+ StringUtils.repeat("*", getPassword().length());
 	}
 }
