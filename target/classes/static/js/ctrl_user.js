a13f5972792d08bf3e3b2d902664c8df7834626a
avaX14 <karacbranko@gmail.com>
2017-06-21 15:30:10 +0200

    Podeseni controlleri

--- target/classes/static/js/ctrl_user.js
+++
@@ -1,4 +0,0 @@
-angular.module('webApp', ['ngRoute']).controller('userController', function($scope, $http, $location, $rootScope) {
-    	
-    	
-});
--- target/classes/static/js/script.js
+++ target/classes/static/js/script.js
@@ -43,6 +43,173 @@
     });
 
     // create the controller and inject Angular's $scope
+    
+    webApp.controller('loginController', function($scope, $http, $location, $rootScope) {
+    	$scope.onLoginClick = function(){
+    		//Guest
+    		if($scope.loginRoll == "1") {
+    			
+    		}
+    		//Waiter
+    		if($scope.loginRoll == "2"){
+    			
+    		}
+    		//Cook
+    		if($scope.loginRoll == "3"){
+    			
+    		}
+    		//Bartender
+    		if($scope.loginRoll == "4"){
+    			
+    		}
+    		//Provider
+    		if($scope.loginRoll == "5"){
+    			
+    		}
+    		//Restaurant manager
+    		if($scope.loginRoll == "6"){
+    			
+    		}
+    		//System manager
+    		if($scope.loginRoll == "7"){
+    			var user = {
+        			firstname:"",
+        			lastname:"",
+        			email:$scope.loginEmail,
+        			password:$scope.loginPassword,
+        			master:0
+            	}
+    			$http.post('/systemManager/login', user).then(
+    				function(response){
+    					//Uspesno
+    					$rootScope.loggedUser = response.data;
+    					console.log("System manager " + response.data.firstname + " " + response.data.lastname + " logged in!");
+    					$location.path('/systemManager/home');
+    				},
+    				function(response){
+    					//Neuspesno
+    					
+    					console.log("LOGIN FAILD");
+    				}
+    			)
+    		}
+    		//User
+    		if($scope.loginRoll == "8"){
+    			var user = {
+    	    			firstname:"",
+    	    			lastname:"",
+    	    			email:$scope.loginEmail,
+    	    			password:$scope.loginPassword
+    	        	}
+    				$http.post('/user/login', user).then(
+    					function(response){
+    						//Uspesno
+    						$rootScope.loggedUser = response.data;
+    						console.log("User " + response.data.firstname + " " + response.data.lastname + " logged in!");
+    						$location.path('/user/home');
+    					},
+    					function(response){
+    						//Neuspesno
+    						
+    						console.log("LOGIN FAILED");
+    					}
+    				)
+    		}
+    		
+    		
+    		
+    		
+    	};
+    	$scope.onRegisterClick = function(){
+    		var user = {
+    			firstName:$scope.firstName,
+    			lastName:$scope.lastName,
+    			email:$scope.registerEmail,
+    			password:$scope.password,
+    			passwordConfirm:$scope.passwordConfirm
+    		}
+    		console.log(user);
+    		$http.post('/user/register', user).then(
+    			function(response){
+    				//Uspesno
+    				
+    				console.log("User " + response.data.firstName + " " + response.data.lastName + " registered!");
+    				$location.path('/user/home');
+    			},
+    			function(response){
+    				//Neuspesno
+    				console.log("REGISTRATION FAILED");
+    			}
+    		)
+    	};
+    });
+    
+    webApp.controller('systemManager_regSysMan_Controller', function($scope, $http, $location, $rootScope){
+    	$scope.regSysMan_btnClick = function(){
+    		var user = {
+    			firstname:$scope.regSysMan_firstname,
+    			lastname:$scope.regSysMan_lastname,
+    			email:$scope.regSysMan_email,
+    			password:$scope.regSysMan_password,
+    			master:0
+    		}
+    		$http.post('/systemManager/addSystemManager', user).then(
+				function(response){
+					$location.path('/systemManager/home');
+				},
+				function(response){
+					console.log("LOGIN FAILD")
+				}
+			)
+    	};
+    });
+    
+    webApp.controller('systemManager_regRes_Controller', function($scope, $http, $location, $rootScope){
+    	$scope.regRes_btnClick = function(){
+    		var res = {
+    			name:$scope.regRes_name,
+    			type:$scope.regRes_type
+    		}
+    		$http.post('/systemManager/addRestaurant', res).then(
+				function(response){
+					console.log("Restaurant " + res.name + " registered.");
+					$location.path('/systemManager/home');
+				},
+				function(response){
+					console.log("Restaurant registration faild.")
+				}
+			)
+    	};
+    });
+    
+    webApp.controller('systemManagerController', function($scope, $http, $location, $rootScope) {
+    	//Samo predefinisani menadzer sistema moze da registruje druge menadzere sistema
+    	if($rootScope.loggedUser.master == 0){
+    		$scope.regSysMBtn = 1;
+    	};
+
+    	$scope.onRegisterRestaurantClick = function(){
+    		$location.path('/systemManager/registerRestaurant');
+    	};
+    	
+    	$scope.onRegisterRestaurantManagerClick = function(){
+    		$location.path('/systemManager/registerRestaurantManager');
+    	};
+    	
+    	$scope.onRegisterSystemManagerClick = function(){
+    		$location.path('/systemManager/registerSystemManager');
+    	};
+    	
+    	$scope.onLogoutClick = function(){
+    		console.log($rootScope.loggedUser.firstname);
+    		$location.path('/');
+    	};
+    });
+    
+    webApp.controller('userController', function($scope, $http, $location, $rootScope) {
+    	
+    	
+    });
    
     
 
